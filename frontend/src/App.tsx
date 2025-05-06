2cfc017e932f4c6e996f4b56143c7a67655cd76e
YouRekt <czarnecki.jurko@gmail.com>
2025-05-06 23:37:34 +0200

    feat: added chat component with mocked functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,51 @@
+import Chat from "@/components/chat";
 import InfoCard from "@/components/info-card";
+import { useState } from "react";
+
+export type Message = {
+	role: "user" | "model";
+	text: string;
+};
+
+const initialMessages: Message[] = [
+	{
+		role: "model",
+		text: "Hello! I am the AI Helpdesk assistant here to help you fill out the form! Let's get started. What is your first name?",
+	},
+	{
+		role: "user",
+		text: "Hello, My name is Andrew",
+	},
+];
 
 function App() {
+	const [messages, setMessages] = useState<Message[]>(initialMessages);
+	const [input, setInput] = useState("");
+	const [isLoading, setLoading] = useState(false);
+
+	const sendMessage = async () => {
+		setLoading(true);
+		setMessages((prev) => [...prev, { role: "user", text: input }]);
+		setInput("");
+		await new Promise((resolve) => setTimeout(resolve, 250));
+		setMessages((prev) => [
+			...prev,
+			{ role: "model", text: input.split("").reverse().join("") },
+		]);
+
+		setLoading(false);
+	};
+
 	return (
-		<div className="flex flex-col items-center p-4 min-h-svh">
+		<div className="flex flex-col items-center p-4 gap-4 max-h-dvh">
 			<InfoCard />
+			<Chat
+				messages={messages}
+				input={input}
+				setInput={setInput}
+				isLoading={isLoading}
+				sendMessage={sendMessage}
+			/>
 		</div>
 	);
 }
