d8f03f85b01585cbcde9e715b1cdb53dd81ad6f5
YouRekt <czarnecki.jurko@gmail.com>
2025-05-07 00:49:36 +0200

    feat: added endpoint that streams chat responses

--- backend/src/routes/chat.ts
+++ backend/src/routes/chat.ts
@@ -1,11 +1,17 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { chatSchema } from "@shared/schemas/chat";
+import { streamText } from "hono/streaming";
+import { streamChat } from "src/lib/gemini";
 
 export const chatRoute = new Hono();
 
-chatRoute.post("/", async (c) => {
-	const { message } = await c.req.json();
+chatRoute.post("/", zValidator("json", chatSchema), async (c) => {
+	const { message, history } = c.req.valid("json");
 
-	console.log(message);
-
-	return c.json({ response: "test response" }, 200);
+	return streamText(c, async (stream) => {
+		for await (const chunk of streamChat(message.text, history)) {
+			await stream.write(chunk!);
+		}
+	});
 });
